97c48a129c37334c453e51d6447315af209e4464
mateusDbarros <mb685212@gmail.com>
2025-04-27 15:45:28 -0300

    uptdate

--- analise.py
+++ analise.py
@@ -5,8 +5,19 @@ df = pd.read_excel('Pasta1.xlsx')
 
 table =  df.head() # Exibe a tabela
 column = df.columns # Exibe as colunas
-produtos = df['Produto'] # Exibe uma parte uma coluna em esprecifico
-vendas = df[['Produto', 'Vendas']] # Exibe mais de uma coluna
+itens = df['Produto'] # Exibe uma parte uma coluna em esprecifico
+produtos = df[['Produto', 'Vendas']] # Exibe mais de uma coluna
+vendas = df['Vendas']
 
+print(table)
 
-print(produtos)
+# Calcula o Total de vendas
+vendas_total = 0
+for venda in vendas:
+    vendas_total += venda
+print(f'Total de vendas: {vendas_total}')
+
+# Calcula o total de vendas por produtos
+eletronicos = df['Produto'] == 'Eletrônico'
+soma = df[eletronicos]['Vendas'].sum()
+print(f'Total de vendas de Eletronicos: {soma}')
